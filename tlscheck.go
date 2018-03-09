fabdb0f3ef8f14bab085217b40b6f47f7d30df28
Thomas Habets <habets@google.com>
2018-03-09 20:41:26 +0000

    Check all addresses

--- tlscheck.go
+++ tlscheck.go
@@ -6,8 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"net"
-	"time"
 	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/sync/semaphore"
@@ -15,62 +15,95 @@ import (
 
 var (
 	concurrency = flag.Int64("concurrency", 100, "Connection concurrency.")
-	timeout     = flag.Duration("timeout", 2*time.Second, "Connection timeout.")
-	warnTime     = flag.Duration("warn_time", 7*24*time.Hour, "Warn if expiring soon.")
+	timeout     = flag.Duration("timeout", 10*time.Second, "Connection timeout.")
+	warnTime    = flag.Duration("warn_time", 7*24*time.Hour, "Warn if expiring soon.")
+	ipv6        = flag.Bool("ipv6", true, "Connect to IPv6 targets too.")
 )
 
-func check(ctx context.Context, host string) error {
+func parse(host string) (string, string, error) {
 	var connectHost, tlsHost string
-	{
-		t := strings.Split(host, "/")
-		if len(t) == 2 {
-			tlsHost, connectHost = t[0],t[1]
-		} else {
-			connectHost = host
-			tlsHost = host
-		}
+	t := strings.Split(host, "/")
+	if len(t) == 2 {
+		tlsHost, connectHost = t[0], t[1]
+	} else {
+		connectHost = host
+		tlsHost = host
 	}
-	
-	
-	ctx2, cancel := context.WithTimeout(ctx, *timeout)
-	defer cancel()
 
-	dialer := net.Dialer{}
-	conn, err := dialer.DialContext(ctx2, "tcp", connectHost)
+	var err error
+	tlsHost, _, err = net.SplitHostPort(tlsHost)
 	if err != nil {
-		return err
+		return "", "", err
 	}
-	defer conn.Close()
+	return connectHost, tlsHost, nil
+}
 
-	h, _, err := net.SplitHostPort(tlsHost)
+func check(ctx context.Context, host string) []error {
+	connectHost, tlsHost, err := parse(host)
 	if err != nil {
-		return err
+		return []error{err}
 	}
-	c := tls.Client(conn, &tls.Config{
-		ServerName: h,
-	})
-	if err := c.Handshake(); err != nil {
-		return fmt.Errorf("handshake error for %q: %v", h, err)
-	}
-	defer c.Close()
 
-	s := c.ConnectionState()
-	for _, cert := range s.PeerCertificates {
-		if time.Now().After(cert.NotAfter) {
-			return fmt.Errorf("%q: Expired cert %q", host, cert.Subject.CommonName)
+	// Dial.
+	h, connectPort, err := net.SplitHostPort(connectHost)
+	if err != nil {
+		return []error{err}
+	}
+	allEndpoints, err := net.LookupHost(h)
+	if err != nil {
+		return []error{err}
+	}
+	var errs []error
+	for _, endpoint := range allEndpoints {
+		// If IPv6 and IPv6 turned off, skip.
+		if !*ipv6 && strings.Contains(endpoint, ":") {
+			log.Debugf("%q Skipping IPv6 address %q", host, endpoint)
+			continue
 		}
-		remaining := cert.NotAfter.Sub(time.Now())
-		if remaining < *warnTime {
-			log.Warningf("%q: Remaining time on %q: %v", host, cert.Subject.CommonName, remaining)
+
+		// Set timeout per connection.
+		ctx2, cancel := context.WithTimeout(ctx, *timeout)
+		defer cancel()
+
+		dialer := net.Dialer{}
+		conn, err := dialer.DialContext(ctx2, "tcp", net.JoinHostPort(endpoint, connectPort))
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+		defer conn.Close()
+
+		// TLS handshake.
+		c := tls.Client(conn, &tls.Config{
+			ServerName: tlsHost,
+		})
+		if err := c.Handshake(); err != nil {
+			return []error{fmt.Errorf("handshake error for %q: %v", tlsHost, err)}
+		}
+		defer c.Close()
+
+		// Check connection state.
+		s := c.ConnectionState()
+		for _, cert := range s.PeerCertificates {
+			if time.Now().After(cert.NotAfter) {
+				errs = append(errs, fmt.Errorf("%q: Expired cert %q", host, cert.Subject.CommonName))
+				continue
+			}
+			remaining := cert.NotAfter.Sub(time.Now())
+			if remaining < *warnTime {
+				log.Warningf("%q: Remaining time on %q: %v", host, cert.Subject.CommonName, remaining)
+			}
 		}
 	}
-	return nil
+	return errs
 }
 
 func main() {
 	flag.Parse()
 	ctx := context.Background()
 	sem := semaphore.NewWeighted(*concurrency)
+
+	// TODO: Do concurrent lookups here, then call concurrent checks.
 	for _, host := range flag.Args() {
 		host := host
 		sem.Acquire(ctx, 1)
@@ -80,8 +113,10 @@ func main() {
 				return
 			}
 
-			if err := check(ctx, host); err != nil {
-				log.Errorf("%q: %v", host, err)
+			if errs := check(ctx, host); len(errs) > 0 {
+				for _, err := range errs {
+					log.Errorf("%q: %v", host, err)
+				}
 			} else {
 				log.Infof("%q: OK", host)
 			}
